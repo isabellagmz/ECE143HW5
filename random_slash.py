b42f02d932d1cd85773e15870bc91869c378de43
isabellagomez <ilgomezt@ucsd.edu>
2020-11-05 12:47:03 -0800

    TRY 3. random_slash.py does what i think it should. Takes into account random column, row beginning and row end. Forgot to do the back on the signature.

--- random_slash.py
+++ random_slash.py
@@ -41,7 +41,7 @@ def gen_rand_slash(m=6,n=6,direction='back'):
             else:
                 break
 
-    # get a random number between 1 and 5 when forward
+    #get a random number between 1 and 5 when forward
     elif direction == 'forward':
         random_column = random.randint(1, 5)
         random_start_row = random.randint(0, 4)
